126c952805276ac221fb74d9964b8e210a298595
PortalFan202 <anibaldonoso1@gmail.com>
2023-02-13 15:02:25 +0200

    Finished V2

--- app/src/main/java/com/example/scaffold/MainActivity.kt
+++ app/src/main/java/com/example/scaffold/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.scaffold.ui.theme.ScaffoldTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,38 +37,78 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ScaffoldApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable(route = "home") {
+            MainApp(navController = navController)
+        }
+        composable(route = "info") {
+            InfoScreen(navController = navController)
+        }
+        composable(route="settings"){
+            SettingScreen(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun MainApp(navController: NavController) {
     Scaffold(
-        topBar = { MyTopBar() },
-        content = { Text(text = "Home screen content") },
-        bottomBar = {
-            BottomAppBar {
-                Text(text = "Bottom Bar")
+        topBar = { MyTopBar(title = "My App", navController = navController) },
+        content = { Text(text = "Content for home page") }
+    )
+}
+
+@Composable
+fun InfoScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar(title = "Info", navController = navController) },
+        content = { Text(text = "Info content") }
+    )
+}
+
+@Composable
+fun SettingScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar(title = "Settings", navController = navController) },
+        content = { Text(text = "Settings content") }
+    )
+}
+
+
+@Composable
+fun MyTopBar(title: String, navController: NavController) {
+    var expanded by remember { mutableStateOf(false) }
+    TopAppBar(
+        title = { Text(text = "My app") },
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.Menu, contentDescription = "Three horizontal lines")
+            }
+        },
+        actions = {
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "Three vertical dots")
+            }
+            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                DropdownMenuItem(onClick = { navController.navigate("info") }) {
+                    Text(text = "Info")
+                }
+                DropdownMenuItem(onClick = { navController.navigate("settings") }) {
+                    Text(text = "Settings")
+                }
             }
         }
     )
 }
 
 @Composable
-fun MyTopBar() {
-    var expanded by remember { mutableStateOf(false) }
+fun ScreenTopBar(title: String, navController: NavController) {
     TopAppBar(
-        title = { Text(text = "My app") },
+        title = { Text(text = title) },
         navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Filled.Menu, contentDescription = null)
-            }
-        },
-        actions = {
-            IconButton(onClick = { expanded = !expanded }) {
-                Icon(Icons.Filled.MoreVert, contentDescription = null)
-            }
-            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-                DropdownMenuItem(onClick = { /*TODO*/ }) {
-                    Text(text = "info")
-                }
-                DropdownMenuItem(onClick = { /*TODO*/ }) {
-                    Text(text = "info2")
-                }
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Back Arrow")
             }
         }
     )
